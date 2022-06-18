64f61dcdae29eff0e9c023b201cd8ce6d47a8f34
Michelangelo Setaro <mksetaro@gmail.com>
2022-06-18 22:56:03 +0200

    Changed iotconfig.py

--- iotsim/config/iotconfig.py
+++ iotsim/config/iotconfig.py
@@ -3,8 +3,8 @@ import os
 import logging
 
 logger ={'logFilePath': os.environ['HOME'] + '/iot_simulator.log',
-         'loggerLevel': logging.DEBUG}
-paths = {'simulator_root': os.environ['HOME'] + '/Workspace/iot-device-simulator/iotsim',
+         'loggerLevel': logging.INFO}
+paths = {'simulator_root': os.environ['HOME'] + '/Workspace/iot-device-simulator',
          'certificates_root_path': os.environ['HOME'] + '/certificates'}
 mqtt_client = { 'host':'localhost',
                 'port': 1883,
@@ -12,5 +12,5 @@ mqtt_client = { 'host':'localhost',
                 'rootCA': paths['certificates_root_path'] + '/ca_certificates/ca.crt',
                 'clientCertificate' : paths['certificates_root_path']  + '/client_certificates/client.crt',
                 'clientKey': paths['certificates_root_path']  + '/client_certificates/client.key'}
-mqtt_modules = { 'iotUnitList': paths['simulator_root'] + '/iot_units/iotunits.json',
-                 'modulesFolder': paths['simulator_root'] + '/iot_units/'}
+mqtt_modules = { 'iotUnitList': paths['simulator_root'] + '/examples/iotunits.json',
+                 'modulesFolder': paths['simulator_root'] + '/examples/'}
